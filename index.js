b5c6a81809c206f4f19fc3f3b3f00ba399386362
Leonard <Leonardminski@hotmail.co.uk>
2023-01-19 11:09:12 +0000

    Endpoints: Loginopolis - complete

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require("bcrypt");
+
+const SALT_COUNT = 7;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   try {
-    res.send('<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>');
+    res.send(
+      "<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>"
+    );
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hashed = await bcrypt.hash(password, 0);
+    await User.create({ username, password: hashed });
+    res.send(`${username} your account is now created.`);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      res.send("User is not found.");
+      return;
+    }
+    // const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(401).send("Username or password is incorrect.");
+      return;
+    }
+
+    res.send(`You have sucessfully logged in ${username}!`);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
